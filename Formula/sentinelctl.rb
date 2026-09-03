c582a50578b353bb24d1ca89ae9e3b55f34d9acd
Muhammad Khan <imkjunejo@gmail.com>
2026-09-03 10:39:23 -0700

    sentinelctl 1.5.0

--- Formula/sentinelctl.rb
+++ Formula/sentinelctl.rb
@@ -1,9 +1,9 @@
 class Sentinelctl < Formula
   desc "Headless mobile app security audit CLI for iOS and Android binaries"
   homepage "https://sentinelden.com/audit"
-  url "https://sentinelden.com/audit/cli/sentinelctl-1.4.0.tar.gz"
-  sha256 "113d549aacdf9084f3de8169ac6085fdb6c242906b85139c3b5c9e674811a440"
-  version "1.4.0"
+  url "https://sentinelden.com/audit/cli/sentinelctl-1.5.0.tar.gz"
+  sha256 "ae28bbafbba9378cbc7f01004e7c8cfaba9cc5dc01618954396a825faf780fe9"
+  version "1.5.0"
   license :cannot_represent
 
   # Universal (arm64 + x86_64), Developer ID signed and notarized.
